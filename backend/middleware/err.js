4b00a5117059cb9be3bd1b68fcd9ec37c9763bdc
Anurag Mourya <anuragengee@gmail.com>
2023-06-28 16:37:32 +0530

    handled Uncaught Exception Error in serve.js and Wrong mongodb Id error or CastError in middleware/err.js

--- backend/middleware/err.js
+++ backend/middleware/err.js
@@ -4,6 +4,13 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;//if err.statusCode is not happen then 500
     err.message = err.message || "Internal Server Error";
 
+    //Wrong mongodb Id error
+    if (err.name === 'CastError') {
+        const message = `Resource not found. Invalid: ${err.path}`;
+        err = new ErrorHandler(message, 400)
+    }
+
+
     res.status(err.statusCode).json({
         success: false,
         // message: err.stack,//for seen the full location of error
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,15 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const connectDatabase = require('./config/database')
 
+//handled Uncaught Exception Error
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Sutting donwn the server due to Unhandled Promise Rejection`);
+    server.close(() => {
+        process.exit(1);
+    })
+})
+
 
 //config
 dotenv.config({ path: 'backend/config/config.env' });
@@ -13,7 +22,7 @@ const server = app.listen(process.env.PORT, () => {
     console.log(`server is started on http://localhost:${process.env.PORT}`);
 });
 
-//Unhandled Promise Rejection
+//Unhandled Promise Rejection error
 process.on("unhandledRejection", (err) => {
     console.log(`Error: ${err.message}`);
     console.log(`Sutting donwn the server due to Unhandled Promise Rejection`);
